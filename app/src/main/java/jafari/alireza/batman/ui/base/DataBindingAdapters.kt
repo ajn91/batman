0ce30b149092334d1dcf53afd075c1da931d4cea
Alireza Jafari <jafari.dev@gmail.com>
2020-09-28 05:12:34 +0330

    add a detail to details page add screenshots

--- app/src/main/java/jafari/alireza/batman/ui/base/DataBindingAdapters.kt
+++ app/src/main/java/jafari/alireza/batman/ui/base/DataBindingAdapters.kt
@@ -1,48 +1,44 @@
-package jafari.alireza.batman.ui.base;
+package jafari.alireza.batman.ui.base
 
-import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.graphics.Typeface
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
 
-import androidx.databinding.BindingAdapter;
-
-import kotlin.jvm.JvmStatic;
-
-public class DataBindingAdapters {
-
+object DataBindingAdapters {
     @BindingAdapter("imageResource")
-    public static void setImageUri(ImageView view, String imageUri) {
+    fun setImageUri(view: ImageView, imageUri: String?) {
         if (imageUri == null) {
-            view.setImageURI(null);
+            view.setImageURI(null)
         } else {
-            view.setImageURI(Uri.parse(imageUri));
+            view.setImageURI(Uri.parse(imageUri))
         }
     }
 
     @BindingAdapter("imageResource")
-    public static void setImageUri(ImageView view, Uri imageUri) {
-        view.setImageURI(imageUri);
+    fun setImageUri(view: ImageView, imageUri: Uri?) {
+        view.setImageURI(imageUri)
     }
 
     @BindingAdapter("imageResource")
-    public static void setImageDrawable(ImageView view, Drawable drawable) {
-        view.setImageDrawable(drawable);
+    fun setImageDrawable(view: ImageView, drawable: Drawable?) {
+        view.setImageDrawable(drawable)
     }
 
     @BindingAdapter("imageResource")
-    public static void setImageResource(ImageView imageView, int resource) {
-        imageView.setImageResource(resource);
+    fun setImageResource(imageView: ImageView, resource: Int) {
+        imageView.setImageResource(resource)
     }
 
     @BindingAdapter("setBold")
     @JvmStatic
-    public static void setBold(TextView view, boolean isBold) {
+    fun setBold(view: TextView, isBold: Boolean) {
         if (isBold) {
-            view.setTypeface(null, Typeface.BOLD);
+            view.setTypeface(null, Typeface.BOLD)
         } else {
-            view.setTypeface(null, Typeface.NORMAL);
+            view.setTypeface(null, Typeface.NORMAL)
         }
     }
 }
